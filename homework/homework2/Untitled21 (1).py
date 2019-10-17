71edebf23cc6e0498c39cdd5cdfea2e520ae9123
chukse <39104359+chukse@users.noreply.github.com>
2019-10-17 01:51:25 -0500

    Add files via upload

---
+++ homework/homework2/Untitled21 (1).py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+list = input("").split(" ")
+
+ 
+
+
+
+list = [int(i) for i in list]
+    
+
+sumy = sum(list)
+
+print("The sum of {} is {}".format(list,sumy))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
