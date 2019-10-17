c67cff770a01ccb469d2bbe5611956ed66d3dd56
chukse <39104359+chukse@users.noreply.github.com>
2019-10-17 02:03:52 -0500

    Add files via upload

---
+++ homework/homework2/Untitled21 (2).py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+list = input("").split(" ")
+
+ 
+
+
+
+list = [int(i) for i in list]
+    
+
+sumy = "sum(list)"
+
+print(eval(sumy))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
